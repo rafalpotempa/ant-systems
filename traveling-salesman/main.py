9449404ade9c26729beb2d174cbd57eb6eabcf93
Rafał Potempa <33723064+rafalpotempa@users.noreply.github.com>
2020-02-08 22:35:16 +0100

    Complete algorithm

--- traveling-salesman/main.py
+++ traveling-salesman/main.py
@@ -3,7 +3,7 @@ from plot import *
 from data import *
 seed(0)
 
-def dist(n1, n2):
+def distance(n1, n2):
 	return ((cities[n1]['x'] - cities[n2]['x'])**2 + (cities[n1]['y'] - cities[n2]['y'])**2)**0.5
 
 def sumTn(ant):
@@ -19,51 +19,73 @@ def sumTn(ant):
 
 alpha = 1
 beta = 5
+ro = 0.5
 x, y = getData()
 n = len(x)
 
 cities = [{'n': i, 'x': x[i], 'y': y[i]} for i in range(n)]
 T = [[1 for i in range(n)] for j in range(n)]
 deltaT = [[1/n for i in range(n)] for j in range(n)]
-d = [[dist(i, j) for i in range(n)] for j in range(n)]
+d = [[distance(i, j) for i in range(n)] for j in range(n)]
 
-ants = [{'init': randrange(0, n), 'path': []} for i in range(n)]
+ants = [{'init': randrange(0, n), 'path': [], 'dist': 0} for i in range(n)]
+bestAnt = ants[0]
 
-for ant in ants:
-	# calculate probability
-	ant['path'].append(ant['init'])
-	for i in range(n-1):
-		a = []
-		for city in cities:
-			current = ant['path'][-1]
-			if city['n'] in ant['path']:
-				continue
-			else:
-				Tj = T[current][city['n']]
-				nj = 1/d[current][city['n']]
-				aj = Tj**alpha * nj**beta / sumTn(ant)
-				a.append({'n': city['n'], 'a': aj})
-		p = []
-		sumA = 0
-		for aj in a:
-			sumA += aj['a']
+for k in range(50):
+	for ant in ants:
+		ant['path'] = [ant['init']]
+		ant['dist'] = 0
+		for i in range(n-1):
+			a = []
+			# find unattended cities
+			for city in cities:
+				current = ant['path'][-1]
+				if city['n'] in ant['path']:
+					continue
+				else:
+					Tj = T[current][city['n']]
+					nj = 1/d[current][city['n']]
+					aj = Tj**alpha * nj**beta / sumTn(ant)
+					a.append({'n': city['n'], 'a': aj})
+			p = []
+			sumA = 0
+			for aj in a:
+				sumA += aj['a']
 
-		for aj in a:
-			p.append({'n': aj['n'], 'p': aj['a']/sumA})
-		
-		r = uniform(0, 1)
-		sumP = 0
-		for pj in p:
-			sumP += pj['p']
-			if r <= sumP:
-				decision = p['n']
+			# calculate probability
+			for aj in a:
+				p.append({'n': aj['n'], 'p': aj['a']/sumA})
+			
+			# make decision
+			r = uniform(0, 1)
+			sumP = 0
+			for pj in p:
+				sumP += pj['p']
+				if r <= sumP:
+					decision = pj['n']
+					ant['path'].append(decision)
+					break
 
+		ant['path'].append(ant['init'])
+		# count deltaT
+		for i, city in enumerate(ant['path'][:-1]):
+			n1 = ant['path'][i]
+			n2 = ant['path'][i+1]
 
-		break
-	break
+			dist = distance(n1, n2)
+			ant['dist'] += dist
 
+			deltaT[n1][n2] += 1/dist
+			deltaT[n2][n1] += 1/dist
 
-	# make decision
+		if bestAnt['dist'] > ant['dist']:
+			bestAnt = ant
 
-# plot(cities)
+	# update pheromone concentration
+	for i, row in enumerate(T):
+		for j, t in enumerate(row):
+			T[i][j] = (1 - ro)*T[i][j] + deltaT[i][j]
+
+print(bestAnt)
+plot(cities, T, bestAnt)
 
--- traveling-salesman/plot.py
+++ traveling-salesman/plot.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 
-def plot(cities):
+def plot(cities, T, bestAnt):
+	Tmax = 0
+	for row in T:
+		for t in row:
+			if t > Tmax:
+				Tmax = t
+
 	for i, city in enumerate(cities):
 		plt.scatter(city['x'], city['y'], c='royalblue')
 		for anotherCity in cities[0:i] + cities[i+1:len(cities)]:
-			plt.plot([city['x'], anotherCity['x']], [city['y'], anotherCity['y']], 'gray', lw=0.5, zorder=0)
+			lw = 0.5 + 3*T[city['n']][anotherCity['n']]/Tmax
+			plt.plot([city['x'], anotherCity['x']], [city['y'], anotherCity['y']], c='gray', lw=lw, zorder=0)
 
+
+	for i in range(len(cities)):
+		n1 = bestAnt['path'][i]
+		n2 = bestAnt['path'][i+1]
+		plt.plot([cities[n1]['x'], cities[n2]['x']], [cities[n1]['y'], cities[n2]['y']], c='orange', zorder=0)
+		
+	for city in cities:
+		plt.annotate(city['n'], (city['x'] + 0.1, city['y'] + 0.1))
 	plt.show()
