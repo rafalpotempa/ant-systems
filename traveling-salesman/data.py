522292202320f898205bf50c03c7b40e50fa2e11
Rafał Potempa <33723064+rafalpotempa@users.noreply.github.com>
2020-02-08 12:40:04 +0100

    Add algorithm

--- traveling-salesman/data.py
+++ traveling-salesman/data.py
@@ -3,8 +3,11 @@ import sys
 
 def getData():
 	lines = []
+	x, y = [], []
 	with open('data/cities1.txt') as file:
 		for line in file:
 			lines.append(re.findall('\d+', line))
-	x, y = lines[1], lines[2]
+
+	x = [int(number) for number in lines[1]]
+	y = [int(number) for number in lines[2]]
 	return x, y
--- traveling-salesman/main.py
+++ traveling-salesman/main.py
@@ -1,18 +1,69 @@
-from random import seed, randrange
+from random import seed, randrange, uniform
 from plot import *
-from data.data import *
+from data import *
 seed(0)
 
+def dist(n1, n2):
+	return ((cities[n1]['x'] - cities[n2]['x'])**2 + (cities[n1]['y'] - cities[n2]['y'])**2)**0.5
+
+def sumTn(ant):
+	sum = 0
+	for city in cities:
+		current = ant['path'][-1]
+		if city['n'] in ant['path']:
+			continue
+		else:
+			nj = 1/d[current][city['n']]
+			sum += T[current][city['n']]**alpha * nj**beta
+	return sum
+
+alpha = 1
+beta = 5
 x, y = getData()
 n = len(x)
 
 cities = [{'n': i, 'x': x[i], 'y': y[i]} for i in range(n)]
-T = [[0 for i in range(n)] for j in range(n)]
+T = [[1 for i in range(n)] for j in range(n)]
 deltaT = [[1/n for i in range(n)] for j in range(n)]
+d = [[dist(i, j) for i in range(n)] for j in range(n)]
 
 ants = [{'init': randrange(0, n), 'path': []} for i in range(n)]
 
 for ant in ants:
-	pass
+	# calculate probability
+	ant['path'].append(ant['init'])
+	for i in range(n-1):
+		a = []
+		for city in cities:
+			current = ant['path'][-1]
+			if city['n'] in ant['path']:
+				continue
+			else:
+				Tj = T[current][city['n']]
+				nj = 1/d[current][city['n']]
+				aj = Tj**alpha * nj**beta / sumTn(ant)
+				a.append({'n': city['n'], 'a': aj})
+		p = []
+		sumA = 0
+		for aj in a:
+			sumA += aj['a']
 
-plot(cities)
+		for aj in a:
+			p.append({'n': aj['n'], 'p': aj['a']/sumA})
+		
+		r = uniform(0, 1)
+		sumP = 0
+		for pj in p:
+			sumP += pj['p']
+			if r <= sumP:
+				decision = p['n']
+
+
+		break
+	break
+
+
+	# make decision
+
+# plot(cities)
+
